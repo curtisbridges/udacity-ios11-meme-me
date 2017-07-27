dfaa13d339f754ec17743425d32c61984274fc35
Curtis Bridges <curtis@curtisbridges.com>
2017-07-27 19:52:24 -0400

    add delegates, handle ui state

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -11,31 +11,96 @@ import UIKit
 class MemeEditorViewController: UIViewController {
 
     // MARK: Outlets
+
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
     @IBOutlet weak var shareButtonItem: UIBarButtonItem!
     @IBOutlet weak var cancelButtonItem: UIBarButtonItem!
-    @IBOutlet weak var topLabel: UILabel!
-    @IBOutlet weak var bottomLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
 
 
     // MARK: - Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        // enable or disable the camera based upon
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+
+    func isUserEditing() -> Bool {
+        return topButton.currentTitle != "TOP" || bottomButton.currentTitle != "BOTTOM" || imageView.image != nil
+    }
+
+    func handleInterfaceState() {
+
+        cancelButtonItem.isEnabled = isUserEditing()
+        shareButtonItem.isEnabled = isUserEditing()
+    }
+
 
     // MARK: - Actions
+
+    @IBAction func changeTopText(_ sender: UIButton) {
+        print("Change top text")
+    }
+
+    @IBAction func changeBottomText(_ sender: UIButton) {
+        print("Change bottom text")
+    }
+
+    // launch the camera app to take a new picture.
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
 
+    // show the album picker to choose an existing image
     @IBAction func showAlbumPicker(_ sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
 
+    // share this meme using the system share sheet
     @IBAction func showShareSheet(_ sender: UIBarButtonItem) {
     }
 
+    // reset the UI
     @IBAction func doCancel(_ sender: UIBarButtonItem) {
+        topButton.titleLabel?.text = "TOP"
+        bottomButton.titleLabel?.text = "BOTTOM"
+        imageView.image = nil
     }
 
 }
 
+// MARK: - Extension UIImagePickerControllerDelegate
+
+extension MemeEditorViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = chosenImage
+        dismiss(animated:true, completion: nil)
+
+        handleInterfaceState()
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+// MARK: - Extension UINavigationControllerDelegate
+
+extension MemeEditorViewController: UINavigationControllerDelegate {
+    // TODO?
+}
