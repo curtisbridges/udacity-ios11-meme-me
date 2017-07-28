15283d0f87fd977201cf6f09232873b8e530e7ba
Curtis Bridges <curtis@curtisbridges.com>
2017-07-27 20:50:31 -0400

    basic layout, proper text, view dodge keyboard

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -10,46 +10,69 @@ import UIKit
 
 class MemeEditorViewController: UIViewController {
 
+    private let initialTopText = "TOP"
+    private let initialBottomText = "BOTTOM"
+
+    private let memeTextAttributes:[String:Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: -1.0]
+
+
     // MARK: Outlets
 
-    @IBOutlet weak var topButton: UIButton!
-    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+
     @IBOutlet weak var shareButtonItem: UIBarButtonItem!
     @IBOutlet weak var cancelButtonItem: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButtonItem: UIBarButtonItem!
 
 
     // MARK: - Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
         // enable or disable the camera based upon
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        cameraButtonItem.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+        // setup text fields
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.text = initialTopText
+        topTextField.textAlignment = .center
+
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.text = initialBottomText
+        bottomTextField.textAlignment = .center
+
+        subscribeToKeyboardNotifications()
     }
 
-    func isUserEditing() -> Bool {
-        return topButton.currentTitle != "TOP" || bottomButton.currentTitle != "BOTTOM" || imageView.image != nil
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
 
-    func handleInterfaceState() {
+    private func isUserEditing() -> Bool {
+        return topTextField.text != initialTopText ||
+                bottomTextField.text != initialBottomText ||
+                imageView.image != nil
+    }
 
+    // fileprivate so extensions can still access
+    fileprivate func handleInterfaceState() {
         cancelButtonItem.isEnabled = isUserEditing()
         shareButtonItem.isEnabled = isUserEditing()
     }
 
 
     // MARK: - Actions
-
-    @IBAction func changeTopText(_ sender: UIButton) {
-        print("Change top text")
-    }
-
-    @IBAction func changeBottomText(_ sender: UIButton) {
-        print("Change bottom text")
-    }
 
     // launch the camera app to take a new picture.
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
@@ -68,18 +91,48 @@ class MemeEditorViewController: UIViewController {
 
     // share this meme using the system share sheet
     @IBAction func showShareSheet(_ sender: UIBarButtonItem) {
+        print("show share sheet")
     }
 
     // reset the UI
     @IBAction func doCancel(_ sender: UIBarButtonItem) {
-        topButton.titleLabel?.text = "TOP"
-        bottomButton.titleLabel?.text = "BOTTOM"
+        topTextField.text = initialTopText
+        bottomTextField.text = initialBottomText
         imageView.image = nil
     }
-
 }
 
-// MARK: - Extension UIImagePickerControllerDelegate
+// MARK: - Extensions
+
+// MARK: Keyboard related extensions
+
+extension MemeEditorViewController {
+    func keyboardWillShow(_ notification:Notification) {
+        view.frame.origin.y = 0 - getKeyboardHeight(notification)
+    }
+
+    func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+}
+
+// MARK: Extension UIImagePickerControllerDelegate
 
 extension MemeEditorViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController,
@@ -99,7 +152,7 @@ extension MemeEditorViewController: UIImagePickerControllerDelegate {
 }
 
 
-// MARK: - Extension UINavigationControllerDelegate
+// MARK: Extension UINavigationControllerDelegate
 
 extension MemeEditorViewController: UINavigationControllerDelegate {
     // TODO?
